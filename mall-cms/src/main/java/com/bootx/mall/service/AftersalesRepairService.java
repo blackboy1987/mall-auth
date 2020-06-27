5fed84fae07801144abb812306c71556265d790f
xial@plian.net <a12345678>
2020-06-27 18:56:04 +0800

    init

---
+++ mall-cms/src/main/java/com/bootx/mall/service/AftersalesRepairService.java
@@ -0,0 +1,14 @@
+
+package com.bootx.mall.service;
+
+import com.bootx.mall.entity.AftersalesRepair;
+
+/**
+ * Service - 维修
+ * 
+ * @author BOOTX Team
+ * @version 6.1
+ */
+public interface AftersalesRepairService extends BaseService<AftersalesRepair, Long> {
+
+}
