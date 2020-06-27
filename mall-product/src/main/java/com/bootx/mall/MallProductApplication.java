6114a9d9555e90d1f58547af42237e7f03aba8ba
xial@plian.net <a12345678>
2020-06-27 19:23:34 +0800

    init

--- mall-product/src/main/java/com/bootx/mall/MallProductApplication.java
+++ mall-product/src/main/java/com/bootx/mall/MallProductApplication.java
@@ -2,8 +2,10 @@ package com.bootx.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages="com.bootx.mall.feign")
 public class MallProductApplication {
 
     public static void main(String[] args) {
