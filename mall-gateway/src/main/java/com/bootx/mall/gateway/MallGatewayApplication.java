2f3cb337e2ff0ea281ed929712c3b26b92be8414
xial@plian.net <a12345678>
2020-06-27 19:20:52 +0800

    init

--- mall-gateway/src/main/java/com/bootx/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/bootx/mall/gateway/MallGatewayApplication.java
@@ -2,7 +2,9 @@ package com.bootx.mall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallGatewayApplication {
 
